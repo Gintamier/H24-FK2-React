10012ea15f5f8705937d10757462466de4a8078c
Gintamier <gintamier@gmail.com>
2024-10-08 20:46:29 +0000

    Yeet

---
+++ shrek-cv/src/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function () {
+  return <div className="bg-green-400 h-40 w-40"></div>;
+}
---
+++ shrek-cv/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-green-400 h-screen w-screen flex flex-col items-center justify-center">
+      <div>
+        <p>Test</p>
+      </div>
+    </div>
+  );
+}
