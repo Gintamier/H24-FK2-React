e74c5f6216c3dd6683884a8bc61c0de980e7d2be
Gintamier <gintamier@gmail.com>
2024-10-09 12:27:15 +0000

    Fixed layout

--- shrek-cv/src/Data/shrek.ts
+++ shrek-cv/src/Data/shrek.ts
@@ -1,12 +1,26 @@
-const SHREK_DATA =  {
-    pfp: "/shrek.jpg",
-    name: "Shrek",
-    description: "Swamp Enthusiast | Ogre Extraordinaire",
-    contact_info: {
-        email: "shrek@swampmail.com",
-        phone: "+123 456 7890",
-        location: "Shrek's Swamp, Far Far Away"
+const SHREK_DATA = {
+  pfp: "/shrek.jpg",
+  name: "Shrek",
+  description: "Swamp Enthusiast | Ogre Extraordinaire",
+  contact_info: {
+    email: "shrek@swampmail.com",
+    phone: "+123 456 7890",
+    location: "Shrek's Swamp, Far Far Away",
+  },
+  skills: [
+    "Swamp Maintenance",
+    "Ogre Roaring",
+    "Onion Layer Analysis",
+    "Donkey Management",
+    "Dragon Communication",
+  ],
+  workexperience: [
+    {
+      title: "Swamp Owner",
+      location: "Far Far Away",
+      period: "2001 - Present",
+      description:
+        "Responsible for maintaining the swamp and keeping unwelcome guests away.",
     },
-    skills: {"Swamp Maintenance", "Ogre Roaring", "Onion Layer Analysis", "Donkey Management", "Dragon Communication"},
-    workexperience: {(title: "Swamp Owner", location:"Far Far Away", period:"2001 - Present", description:"Responsible for maintaining the swap and keeping unwelcome guests away.")}
-}
+  ],
+};
